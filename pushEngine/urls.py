735db3d9b99cc248374eb6958c8374c9ea302885
dhdiagram4011 <dhdiagram@gmail.com>
2020-10-12 21:16:18 +0900

    pushEngine view page coding

---
+++ pushEngine/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from searchEngine import views
+
+
+urlpattenrs = [
+    path('send/', send, name=send),
+    path('sendList/', sendList, name=sendList),
+    path('revList/', revList, name=revList),
+]
+
+
+
+
--- pushEngine/views.py
+++ pushEngine/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .models import MsgPush
 
-# Create your views here.
+#### urlconf List
+# path('send/', send, name=send),
+# path('sendList/', sendList, name=sendList),
+# path('revList/', revList, name=revList),
+
+
+def send(request):
+    if request.method == 'GET':
+        ### action
+        form = sendForm(request.GET)
+        return render(request, 'pushEngine/send.html', {'form':form})
+    elif request.method == 'POST':
+        form = sendForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            ### model field save() : send, receiver, title , text
+            send = request.POST['send']
+            receiver = request.POST['receiver']
+            title = request.POST['title']
+            text = request.POST['text']
+            post.save()
+        return render(request, 'pushEngine/send_success.html')
+        
+
+
+def sendList(request):
+    if request.method == 'POST':
+        form = sendListForm(request.POST)    
+        ### action
+
+    else:
+
+
+
+def revList(request):
+    if request.method == 'POST':
+        form = revListForm(request.POST)
+        ### action
+    else:
+
+
