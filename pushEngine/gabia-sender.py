3129bff57dcbaf5a878f82e98d02f5efe34a1199
dhdiagram4011 <dhdiagram@gmail.com>
2020-10-28 21:19:35 +0900

    sms token auth test

--- pushEngine/gabia-sender.py
+++ pushEngine/gabia-sender.py
@@ -1,26 +1,19 @@
 ### key : da740db95614eda666e49b3c867a0790
 
+# # before sender auth
 import requests
 import time
 
 url = 'https://sms.gabia.com/oauth/token'
 payload = 'grant_type=client_credentials'
 headers = {
-    'Content-Type':'application/x-www-form-urlencoded',
-    'Authorization':'Basic 3eef0591ba8d19d91f5e64731893e7ec'
+'Content-Type':'application/x-www-form-urlencoded',
+'Authorization':'Basic fdc1cb65bdcb6b2629bb8104593697dc'
 }
-response = requests.request('POST', url, headers = headers, data = payload, allow_redirects=False, timeout=1)
+#response = requests.request('POST', url, headers=headers, data=payload, allow_redirects=False)
+response = requests.request('POST', url, headers=headers, data=payload, allow_redirects=False)
+print(response)
+print(response.status_code)
 print(response.text)
 
 
-# import requests
-# import time
-
-# url = 'https://sms.gabia.com/api/send/sms'
-# payload = 'phone=01021764011&callback=010217640110&message=SMS%20TEST%20MESSAGE&refkey=[[RESTAPITEST20201026]]'
-# headers = {
-#     'Content-Type':'application/x-www-form-urlencoded',
-#     'Authorization':'Basic DckviEksLs6ZXlKMGVYQWlPaUpLVhiR2NpT2lKU1V6STFOaUo5LmV5SnBjM01pT2lKb2RIUndjenBjTDF3dmMyMXpMbWRoWW1saExtTnZiVnd2SWl3aVlYVmtJam9pWEM5dllYVjBhRnd2ZEc5clpXNGlMQ0pshWFhnT2pBNG5uVkVuLWtnVEJoRGpPeWc='
-# }
-# response = requests.post(url, headers=headers, data=payload, allow_redirects=False, timeout='3')
-# print(response.text)
